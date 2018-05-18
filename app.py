93c69f8dd6c13e239e27afab70effe1e7eb64335
Richard Doell <rfdoell@gmail.com>
2018-05-17 20:24:52 -0600

    Update to satisfy pylint

--- app.py
+++ app.py
@@ -9,8 +9,15 @@ the index.html as a simple cut.
 
 from flask import Flask, render_template
 
+#pylint: disable=invalid-name
 app = Flask(__name__)
 
 @app.route('/')
 def serve_index():
+    '''
+    This function will render the default html page and provide it to the user.
+
+    :return: A rendered version of the index page
+    :rtype: flask.Response
+    '''
     return render_template('index.html')
